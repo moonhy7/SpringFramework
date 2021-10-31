c778865516dc73c451262b19ccc1618a47fa6736
moonhy7 <moonhy7@naver.com>
2021-10-31 11:39:08 +0900

    21.10.31.Sun

--- _053_BoardWeb_MVC_Interface/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
+++ _053_BoardWeb_MVC_Interface/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
@@ -15,7 +15,6 @@ public class AfterThrowingAdvice {
 	@AfterThrowing(pointcut="allPointcut()", throwing = "exceptionObj")
 	public void afterThrowingAdvice(JoinPoint jp, Exception exceptionObj) {
 		String method = jp.getSignature().getName();
-		
 		System.out.println("[예외 처리] " + method + "() 메소드 수행 중 발생한 예외 메시지" + exceptionObj.getMessage());
 	}
 }
